3bb6acb6dc0f24515ac47d1d66d3e1e042044e2e
jdalebrook <dalebrook00@gmail.com>
2021-10-15 09:24:39 +0200

    CleanCodeAndSolid
    Clean code and SOLID recomendations

---
+++ pattern/cleancode.js
@@ -0,0 +1,20 @@
+const cleancode = `
+Codigo Limpio
+
+Nombres auto descriptivos
+Elimina datos poco relevantes de los nombres, (List, Array…)
+Elimina Datos (The, Data, Variable...)
+Nombres pronunciables yyddmm > currentData
+Crea constantes reutilizables en vez de hardcodear números MAX_CLASSES_PER_STUDENT
+Crea clases reutilizables (Get, Fetch…)
+Funciones de máximo 20 líneas
+Chequea que hacen lo que prometen
+Crea funciones con los condicionales
+Dos argumentos como máximo
+Cada función corresponde a una sóla tarea
+Revisa los efectos indeseados
+No repitas funcionalidades
+Borra todos los comentarios, añadelo al control de versiones
+Entiende el lenguaje que estás usando y usa las convenciones
+`
+console.log(cleancode);
---
+++ pattern/solid.js
@@ -0,0 +1,9 @@
+ 
+const solid = `
+S — Single-responsibility principle > Cada class debe tener sólo una funcionalidad
+O — Open-closed principle > Los objetos deben poderse expandir pero no modificar
+L — Liskov substitution principle > Las pruebas del padre sobre el hijo deben de funcionar
+I — Interface segregation principle > Se debe usar interfaces mínimas
+D — Dependency Inversion Principle > 
+`
+console.log(solid);
